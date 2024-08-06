1cb3d223343ad89457dfa375404c15bf4da6fa37
github-usman <usmantoyou@gmail.com>
2024-08-06 19:07:23 +0530

    img compressed : for perfomarance.

--- client/src/components/landing-page/Hero.jsx
+++ client/src/components/landing-page/Hero.jsx
@@ -1,15 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import phone__and_couple from './../../assets/hero/phone__and_couple.png';
 import money_1 from './../../assets/hero/money_1.png';
 import money_2 from './../../assets/hero/money_2.png';
 import money_3 from './../../assets/hero/money_3.png';
 import money_4 from './../../assets/hero/money_4.png';
 import money_5 from './../../assets/hero/money_5.png';
+import phone__and_couple from './../../assets/hero/phone__and_couple.avif';
+import placeholderImage from './../../assets/hero/phone__and_couple_x1.png';
 
 const Hero = () => {
+  const [imageLoaded, setImageLoaded] = useState(false);
+
+  const handleImageLoad = () => {
+    setImageLoaded(true);
+  };
+
   return (
-    <div className="custom-template custom-w  relative  m-auto   md:h-[90%] items-center flex-col gap-3 lg:gap-[8rem] md:flex-row justify-center ">
+    <div className="custom-template custom-w relative m-auto md:h-[90%] items-center flex-col gap-3 lg:gap-[8rem] md:flex-row justify-center">
       <img
         src={money_1}
         className="h-auto max-w-[140px] w-full absolute top-0 start-0"
@@ -25,9 +32,9 @@ const Hero = () => {
         className="h-auto max-w-[140px] w-full absolute top-0 end-0"
         alt="money on3"
       />
-      <div className="flex flex-col gap-3 lg:gap-[40px]  mt-[4rem] lg:mt-0  p-0 lg:p-2  items-center sm:items-start ">
+      <div className="flex flex-col gap-3 lg:gap-[40px] mt-[4rem] lg:mt-0 p-0 lg:p-2 items-center sm:items-start">
         <div className="bg-inherit">
-          <h1 className='text-[2.05rem]  w-full lg:text-[3.4rem] mxl:text-[3.8rem] font-[700] font-["roboto"] lg:text-nowrap bg-inherit leading-none'>
+          <h1 className='text-[2.05rem] w-full lg:text-[3.4rem] mxl:text-[3.8rem] font-[700] font-["roboto"] lg:text-nowrap bg-inherit leading-none'>
             1000+ pages in
             <br className="hidden lg:inline" />
             one Minute
@@ -49,7 +56,7 @@ const Hero = () => {
           Create Pages
         </Link>
       </div>
-      <div className="bg-[#0c0a0a00] transition-transform duration-300  w-[100%] lg:w-[58%] lg:h-[100%]">
+      <div className="bg-[#0c0a0a00] transition-transform duration-300 w-[100%] lg:w-[58%] lg:h-[100%]">
         <img
           src={money_4}
           className="h-auto max-w-[140px] w-full absolute z-10 bottom-0 md:bottom-[4%] right-[70%] md:right-[35%]"
@@ -63,9 +70,10 @@ const Hero = () => {
         />
         <div className="bg-inherit flex justify-start items-center h-[120%] lg:w-[120%]">
           <img
-            src={phone__and_couple}
+            src={imageLoaded ? phone__and_couple : placeholderImage}
             alt="phone and couple"
-            className="object-contain z-0 bg-inherit "
+            className="object-contain z-0 bg-inherit"
+            onLoad={handleImageLoad}
           />
         </div>
       </div>
